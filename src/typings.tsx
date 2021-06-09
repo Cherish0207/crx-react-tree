b980ddd31a4e4c2bde6fd7fda1bbd0e92a40f197
Cherish0207 <cheshire_1207@foxmail.com>
2021-06-09 15:21:39 +0800

    readme.md

--- src/typings.tsx
+++ src/typings.tsx
@@ -1,9 +1,9 @@
 // 声明一个接口,名字叫 Treedata , 定义一个变量的类型,定义对象的属性名和属性的类型
 export interface TreeData {
   name: string;
-  key: string;
+  key: string; // 1.dom-diff 2.生成映射表的key
   type: string;
-  collapsed: boolean;
+  collapsed: boolean; // 是否折叠
   children?: TreeData[]; // ?:表示可选属性,可以给也可以不给
   parent?: TreeData;
   checked?: boolean;
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   devtool: "source-map",
   devServer: {
     hot: true,
+    port: 8001,
     contentBase: path.join(__dirname, "dist"),
     historyApiFallback: {
       //重定向
